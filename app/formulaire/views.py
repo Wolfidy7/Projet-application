c59b7fe8f3a3cdf4b8dcc3bc627107e650f74c01
ibtissam jennate <ibtissamjennate67@gmail.com>
2023-06-19 09:35:26 +0200

    fichier compilation

--- app/formulaire/views.py
+++ app/formulaire/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from formulaire.models import Data
 from formulaire.forms import SendWork
-
+from formulaire.compilation import process_zip 
 def hello(request):
     
     datas = Data.objects.all()
@@ -10,31 +10,32 @@ def hello(request):
                   {'first':datas[0]})
     
 
+
 def submit(request):
-    
-    
-    if request.method =='POST':
-        form = SendWork(request.POST, request.FILES) #ajout du formulaire
-        
+    if request.method == 'POST':
+        form = SendWork(request.POST, request.FILES)
+
         if form.is_valid():
-            
             fichier = request.FILES['file']
             nom = request.POST.get('name')
             print(fichier)
             print(nom)
-            
+
             # Enregistrer le nom du fichier et le fichier dans la base de données
-            data = Data(name=nom, file= fichier)
+            data = Data(name=nom, file=fichier)
             data.save()
+
+            # Décompresser et compiler les fichiers
+            destination_directory = 'Projet-application/app/files'
+            process_zip(fichier, destination_directory)
+
             return redirect('redirection')
-    
+
     else:
         form = SendWork()
-        
-    return render(request,
-                  'formulaire/submit.html',
-                  {'formulaire': form}) #passe le formulaire au gabarit
-    
+
+    return render(request, 'formulaire/submit.html', {'formulaire': form})
+
     
     
     
