241c81f5ed17393270fa08145fabb7688e7d9933
smakalou <makaloucherif@gmail.com>
2023-06-20 11:21:31 +0200

    Ajout de l'option inscription

--- fotoblog/authentication/models.py
+++ fotoblog/authentication/models.py
@@ -1,18 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 
 class User(AbstractUser):
-    
     PROFESSOR = 'PROFESSOR'
     STUDENT = 'STUDENT'
-    
-    ROLES_CHOICES =(
+
+    ROLE_CHOICES = (
         (PROFESSOR, 'Professeur'),
         (STUDENT, 'Etudiant'),
     )
-    
-    profile_photo = models.ImageField()
-    role = models.CharField(max_length=30, choices=ROLES_CHOICES)
-    
+    profile_photo = models.ImageField(verbose_name='photo de profil')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='rôle')
